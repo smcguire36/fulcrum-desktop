93e110a181669966443137d5b4aa59e5c44c8005
Zac McCormick <zac.mccormick@gmail.com>
2017-04-15 09:23:17 -0400

    Add plugin to generate PDF's for each record. Disable by default.

---
+++ plugins/reports/plugin.js
@@ -0,0 +1,29 @@
+import Plugin from '../../src/plugin';
+import Generator from '../../src/reports/generator';
+import mkdirp from 'mkdirp';
+import path from 'path';
+
+const REPORT_PATH = path.join('.', 'reports');
+
+export default class ReportPlugin extends Plugin {
+  // return true to enable this plugin
+  get enabled() {
+    return false;
+  }
+
+  async initialize({app}) {
+    mkdirp.sync(REPORT_PATH);
+
+    app.on('record:save', this.onRecordSave);
+  }
+
+  onRecordSave = async ({record}) => {
+    const generator = new Generator(record);
+
+    const reportDirectory = path.join(REPORT_PATH, record.form.name);
+
+    mkdirp.sync(reportDirectory);
+
+    await generator.generate(reportDirectory);
+  }
+}
