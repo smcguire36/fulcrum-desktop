fb78c0fe289fc40bcf56cc7518e58021f7354577
Zac McCormick <zac.mccormick@gmail.com>
2017-04-27 10:32:55 -0400

    Install some more packages

--- setup-ubuntu.sh
+++ setup-ubuntu.sh
@@ -4,4 +4,22 @@ echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/source
 
 curl -sL https://deb.nodesource.com/setup_7.x | sudo -E bash -
 
-sudo apt-get update && sudo apt-get install nodejs yarn
+sudo apt-get update
+
+sudo apt-get install -y \
+  git \
+  wget \
+  curl \
+  python-software-properties \
+  autoconf \
+  bison \
+  flex \
+  libtool \
+  make \
+  build-essential \
+  binutils-doc \
+  vim \
+  zip \
+  libpq-dev \
+  nodejs \
+  yarn
