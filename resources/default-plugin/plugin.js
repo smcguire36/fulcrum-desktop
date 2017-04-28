0fe732626f9f58c3c46e1446286582d2b9443ec5
Zac McCormick <zac.mccormick@gmail.com>
2017-04-28 15:31:01 -0400

    Update default plugin

--- resources/default-plugin/plugin.js
+++ resources/default-plugin/plugin.js
@@ -1,5 +1,21 @@
 export default class {
-  async task() {
+  async task(cli) {
+    return cli.command({
+      command: 'example',
+      desc: 'an example command',
+      builder: {
+        org: {
+          desc: 'organization name',
+          required: true,
+          type: 'string'
+        }
+      },
+      handler: this.runCommand
+    });
+  }
+
+  runCommand = async () => {
+    console.log(fulcrum.args.org);
   }
 
   async activate() {
