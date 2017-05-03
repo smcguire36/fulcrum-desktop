48e4fe9e387ed07220f59ee6fb622d0e0cb8a132
Zac McCormick <zac.mccormick@gmail.com>
2017-05-03 19:16:22 -0400

    Add linux installation script

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+apt-get update
+
+GITHUB_FEED="https://api.github.com/repos/fulcrumapp/fulcrum-desktop/releases/latest"
+
+VERSION=$(curl -s "$GITHUB_FEED" | grep '"name":' | head -n 1 | sed -e '1s/  "name": "//' | sed -e '1s/",//')
+
+DEB_FILE="Fulcrum_${VERSION}_amd64.deb"
+
+DOWNLOAD_URL="https://github.com/fulcrumapp/fulcrum-desktop/releases/download/v${VERSION}/${DEB_FILE}"
+
+cd ~
+
+curl -L $DOWNLOAD_URL -o $DEB_FILE
+
+dpkg -i $DEB_FILE
+
+apt-get install -f
+
+echo "Fulcrum successfully installed to /opt/Fulcrum"
+
+/opt/Fulcrum/scripts/fulcrum setup
