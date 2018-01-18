89f47cea38d8debf557518ed6ae9a9842cab8d50
Zac McCormick <zac.mccormick@gmail.com>
2018-01-18 18:31:38 -0500

    Add --form <id> --form <id> --form <id> support to sync command to limit the forms that are sync'd.

--- src/main/sync/tasks/download-all-records.js
+++ src/main/sync/tasks/download-all-records.js
@@ -4,13 +4,19 @@ import DownloadPhotos from './download-photos';
 import DownloadVideos from './download-videos';
 import DownloadAudio from './download-audio';
 import DownloadSignatures from './download-signatures';
-// import DownloadPhotos from './download-photos';
+import app from '../../app';
 
 export default class DownloadAllRecords extends Task {
-  async run({account, dataSource}) {
-    const forms = await account.findActiveForms();
+  async run({dataSource}) {
+    const forms = await this.account.findActiveForms();
+
+    const includedForms = this.includedForms;
 
     for (const form of forms) {
+      if (includedForms != null && includedForms.indexOf(form.id) === -1) {
+        continue;
+      }
+
       await new Promise((resolve, reject) => {
         form.load(dataSource, resolve);
       });
@@ -23,5 +29,13 @@ export default class DownloadAllRecords extends Task {
     this.synchronizer.addTask(new DownloadVideos(this.synchronizer.taskParams));
     this.synchronizer.addTask(new DownloadAudio(this.synchronizer.taskParams));
     this.synchronizer.addTask(new DownloadSignatures(this.synchronizer.taskParams));
+  }
+
+  get includedForms() {
+    if (app.args.form) {
+      return Array.isArray(app.args.form) ? app.args.form : [ app.args.form ];
+    }
+
+    return null;
   }
 }
